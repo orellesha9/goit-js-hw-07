3ed5437cad0884a7bbfb88d778c61211e8656c50
orellesha9 <orellesha9@gmail.com>
2023-10-29 01:28:34 +0300

    finish_1_task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const ulElement = document.querySelector(".gallery");
+ulElement.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+ulElement.addEventListener("click", handleClick);
 
-console.log(galleryItems);
+function handleClick(event) {
+    event.preventDefault();
+    if (event.target === event.currentTarget) {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+instance.show();
+
+document.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+            instance.close()
+        }
+    })
+    
+}
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}

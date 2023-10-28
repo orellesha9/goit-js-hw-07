39c14c90d048f5b5b2a4dcc7eff02f631aeb2988
orellesha9 <orellesha9@gmail.com>
2023-10-29 02:05:56 +0300

    start_2_task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const ulElement = document.querySelector(".gallery");
+ulElement.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+ulElement.addEventListener("click", handleClick);
 
-console.log(galleryItems);
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+    const instance = new SimpleLightbox(galleryItems);
+    instance.open();
+
+//   document.addEventListener("keydown", (event) => {
+//     if (event.code === "Escape") {
+//       instance.close();
+//     }
+//   });
+}
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}

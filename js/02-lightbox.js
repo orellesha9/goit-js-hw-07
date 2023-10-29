ae8a24c78f77ae79178701052f2a046028aa5f57
orellesha9 <orellesha9@gmail.com>
2023-10-29 18:07:20 +0200

    finish_task_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,35 +2,22 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const ulElement = document.querySelector(".gallery");
 ulElement.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
-ulElement.addEventListener("click", handleClick);
 
-function handleClick(event) {
-  event.preventDefault();
-  if (event.target === event.currentTarget) {
-    return;
-  }
-    const instance = new SimpleLightbox(galleryItems);
-    instance.open();
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt", // атрибут значення якого буде показано
+  captionDelay: 250  // затримка перед тим як показувати текст
+});
 
-//   document.addEventListener("keydown", (event) => {
-//     if (event.code === "Escape") {
-//       instance.close();
-//     }
-//   });
-}
+// Функція для створення розмітки
 function createMarkup(arr) {
   return arr
     .map(
       ({ preview, original, description }) => `<li class="gallery__item">
-  <a class="gallery__link" href="${preview}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
 </li>`
     )
     .join("");
 }
+

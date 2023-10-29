d3ab3ea779e1323cdec7c994ecc143df54439e99
orellesha9 <orellesha9@gmail.com>
2023-10-29 16:41:04 +0200

    fix_error_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,31 +2,45 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const ulElement = document.querySelector(".gallery");
 ulElement.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+// Додаємо обробник кліку на всю галерею
 ulElement.addEventListener("click", handleClick);
-
-function handleClick(event) {
-    event.preventDefault();
-    if (event.target === event.currentTarget) {
-        return;
+// Функція для відкриття модального вікна
+function openLightbox(imageSource) {
+  const instance = basicLightbox.create(
+    `
+        <img src="${imageSource}" width="800" height="600">
+    `,
+    {
+      onShow: (instance) => {},
+      onClose: (instance) => {},
     }
-    const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`)
+  );
 
-instance.show();
+  instance.show();
 
-document.addEventListener("keydown", (event) => {
-        if (event.code === "Escape") {
-            instance.close()
-        }
-    })
-    
+  // Додаємо обробник клавіші "Escape" для закриття модального вікна
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
 }
+
+// Обробник кліку на зображення
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target.tagName === "IMG") {
+    const imageSource = event.target.dataset.source;
+    openLightbox(imageSource);
+  }
+}
+
+// Функція для створення розмітки
 function createMarkup(arr) {
   return arr
     .map(
       ({ preview, original, description }) => `<li class="gallery__item">
-  <a class="gallery__link" href="${preview}">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -38,3 +52,4 @@ function createMarkup(arr) {
     )
     .join("");
 }
+
